802493904d8c795ab14f332cb88c3ff961a5b7b3
rahym2111 <152060438+rahym2111@users.noreply.github.com>
2026-07-23 08:11:53 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render serweriniň hödürleýän PORT üýtgeýänini alýarys (bolmasa 5000)
+    port = int(os.environ.get('PORT', 5000))
+    
+    # host='0.0.0.0' Render-iň porty daşaryga açmagy üçin MÜTLEM GEREK!
+    app.run(host='0.0.0.0', port=port)
