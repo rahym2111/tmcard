a04650b083b4e67220e224610ce1bb2315e6a662
rahym2111 <152060438+rahym2111@users.noreply.github.com>
2026-07-23 08:16:49 +0500

    Update run.py

--- run.py
+++ run.py
@@ -4,8 +4,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # Render serweriniň hödürleýän PORT üýtgeýänini alýarys (bolmasa 5000)
     port = int(os.environ.get('PORT', 5000))
-    
-    # host='0.0.0.0' Render-iň porty daşaryga açmagy üçin MÜTLEM GEREK!
-    app.run(host='0.0.0.0', port=port)
+    # debug=True etsek, ýalňyşlyk gelende göni ekranda sebäbi ýazylar!
+    app.run(host='0.0.0.0', port=port, debug=True)
